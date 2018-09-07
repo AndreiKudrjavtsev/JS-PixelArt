e8f69828b6996156cccf33ff74b2bb55bf5fb987
Andrey Kudryavtsev <tmac1attack@gmail.com>
2018-09-07 19:17:38 +0300

    core functionality
    Added color change on grid with 3 available colors

--- src/main.js
+++ src/main.js
@@ -12,4 +12,27 @@ function makeGrid(height, width) {
     }
 }
 
+function setColor() {
+
+}
+
+
+
 makeGrid(10,10);
+
+var currentColor;
+var currentColorButton = document.getElementById('current-color');
+var colorButtons = document.querySelectorAll('div.color');
+var cells = document.querySelectorAll('.cell');
+var cells2 = document.getElementsByClassName('cell');
+
+
+colorButtons.forEach(elem => elem.addEventListener("click", function () {
+    let clickedButtonStyle = window.getComputedStyle(elem);
+    currentColor = clickedButtonStyle.getPropertyValue('background-color');
+    currentColorButton.style.backgroundColor = currentColor;
+}))
+
+cells.forEach(cell => cell.addEventListener("click", function () {
+    cell.style.backgroundColor = currentColor;
+}))
