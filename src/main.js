6112196cc1aa93966096ba4ec44a37c02fc0d3c2
Andrey Kudryavtsev <tmac1attack@gmail.com>
2018-09-09 23:05:59 +0300

    portion of styling
    Added styling for colors pallete; working on grid

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ function makeGrid(height, width) {
     let grid = document.getElementById('grid');
     for (var i = 0; i < height; i++) {
         let row = document.createElement('div');
-        row.className = 'row';
+        row.className = 'grid_row';
         for (var j = 0; j < width; j++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
@@ -12,27 +12,21 @@ function makeGrid(height, width) {
     }
 }
 
-function setColor() {
-
-}
-
-
-
+// main fucntionality of pixel art goes here;
 makeGrid(10,10);
 
 var currentColor;
 var currentColorButton = document.getElementById('current-color');
 var colorButtons = document.querySelectorAll('div.color');
 var cells = document.querySelectorAll('.cell');
-var cells2 = document.getElementsByClassName('cell');
 
-
+// event handling for switching current color
 colorButtons.forEach(elem => elem.addEventListener("click", function () {
     let clickedButtonStyle = window.getComputedStyle(elem);
     currentColor = clickedButtonStyle.getPropertyValue('background-color');
     currentColorButton.style.backgroundColor = currentColor;
 }))
-
+// even handling for painting
 cells.forEach(cell => cell.addEventListener("click", function () {
     cell.style.backgroundColor = currentColor;
 }))
